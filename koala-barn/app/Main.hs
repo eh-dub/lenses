f5caf90204168c00f490c0a6ba14fea5c25252a2
asweingarten <asweingarten@gmail.com>
2018-02-25 23:50:26 -0700

    Working on CSS grid code gen

--- koala-barn/app/Main.hs
+++ koala-barn/app/Main.hs
@@ -10,6 +10,10 @@ main = do
   f <- readFile "../bootstrapStyle/index.html"
   let grids = retrieveBootstrapGrids f
   let grid1 = head grids
-  putStrLn $ show $ grid1
-  putStrLn $ renderTags $ grid1
-  putStrLn $ show $ bootstrapToGrid grid1
+  -- putStrLn $ show $ grid1
+  -- putStrLn $ renderTags $ grid1
+  let nativeGrid = bootstrapToGrid grid1
+  -- putStrLn $ show $ nativeGrid
+  putStrLn $ show $ fmap (renderTags . snd) <$> nativeGrid
+  putStrLn $ show $ fmap (fst) <$> nativeGrid
+  putStrLn $ show $ assignIds nativeGrid
--- koala-barn/src/Grid.hs
+++ koala-barn/src/Grid.hs
@@ -3,35 +3,56 @@
 module Grid where
 
 import Text.HTML.TagSoup hiding (Row, Column)
+
 import ParseBootstrap
 
 import Data.Traversable (for)
 import Data.Maybe (fromJust)
 
--- Grid∷
---  rows
---   ratio of columns 4 4 4
+type Grid a = [[a]]
 
-data Column a =
-  Column {weight::Int, contents::a}
-  deriving (Show, Eq, Functor)
+type BootstrapGrid = Grid (Int, [Tag String])
+type CSS = String
+type NativeGrid = Grid (CSS, [Tag String])
 
-data Row a =
-  Row [Column a]
-  deriving (Show, Eq, Functor)
-
-data Grid a =
-  Grid [Row a]
-  deriving (Show, Eq, Functor)
-
-type BootstrapGrid = Grid [Tag String]
-
--- for
---   :: (Applicative f, Traversable t) => t a -> (a -> f b) -> f (t b)
-bootstrapToGrid :: [Tag String] -> Grid ([Tag String])
+bootstrapToGrid :: [Tag String] -> Grid (Int, [Tag String])
 bootstrapToGrid bootstrapGrid =
-  Grid
-  $ flip fmap (rowsFromGrid bootstrapGrid) $ \row ->
-    Row $ flip fmap (columnsFromRow row) $ \column ->
-      Column {weight = fromJust $ columnWeight $ head column
-             ,contents = column}
+  flip fmap (rowsFromGrid bootstrapGrid) $ \row ->
+    flip fmap (columnsFromRow row) $ \column ->
+      (fromJust $ columnWeight $ head column, column)
+
+assignIds :: Grid a -> Grid ((Int, Int), a)
+assignIds =
+  overGrid (zip [0..])
+           (zip [0..] . snd)
+    $ \(i,row) (j, col) ->
+        ((i,j), col)
+
+
+incorporateIds :: Grid ((Int, Int), [Tag String]) -> Grid [Tag String]
+incorporateIds =
+  overGrid id
+           id
+    $ \_ ((i, j), column) ->
+       addId i j (head column) : tail column
+       where
+         -- overwriting ideas is a Bad Idea
+         addId i j (TagOpen tagName attrs) = TagOpen tagName $ ("id", mconcat ["blob", "-", show i, "-", show j] ) : attrs
+
+-- @TODO: use assignIds and incorporateIds and print the result
+
+overGrid :: (Grid a -> [row])
+         -> (row -> [col])
+         -> (row -> col -> b)
+         -> Grid a
+         -> Grid b
+overGrid rowF colF doF grid =
+  flip fmap (rowF grid) $ \row ->
+    flip fmap (colF row) $ \column ->
+      doF row column
+
+-- .grid {
+--   display: grid;
+  -- grid-template-rows: minmax(10px, auto);
+--   grid-template-columns: Xfr, Yfr, Zfr, ...
+-- }
